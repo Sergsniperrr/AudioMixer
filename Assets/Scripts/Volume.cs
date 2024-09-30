871df07a6158db11cab819eec782c9f5f9f35a31
Sergsniperrr <153460753+Sergsniperrr@users.noreply.github.com>
2024-09-30 22:21:27 +0300

    Correction_1

--- Assets/Scripts/Volume.cs
+++ Assets/Scripts/Volume.cs
@@ -1,21 +1,28 @@
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.Audio;
 
 public class Volume : MonoBehaviour
 {
+    [SerializeField] private Slider _slider;
+    [SerializeField] private Toggle _toggle;
     [SerializeField] private AudioMixerGroup _mixerGroup;
     [SerializeField] private string _mixerVariableName;
 
     private float _currentVolume;
-    private float _muteSoundsValue = -85f;
+    private float _muteSoundsValue = -80f;
+
+    private void Awake()
+    {
+        _slider.onValueChanged.AddListener(ChangeValue);
+        _toggle.onValueChanged.AddListener(ToggleAllSounds);
+    }
 
     public void ChangeValue(float volume)
     {
         int volumeMultiplier = 20;
 
-        _mixerGroup.audioMixer.GetFloat(_mixerVariableName, out float currentVolume);
-
-        if (currentVolume != _muteSoundsValue)
+        if (_toggle.enabled)
             _mixerGroup.audioMixer.SetFloat(_mixerVariableName, Mathf.Log10(volume) * volumeMultiplier);
     }
 
@@ -24,11 +31,13 @@ public class Volume : MonoBehaviour
         if (enabled)
         {
             _mixerGroup.audioMixer.SetFloat(_mixerVariableName, _currentVolume);
+            _slider.interactable = true;
         }
         else
         {
             _mixerGroup.audioMixer.GetFloat(_mixerVariableName, out _currentVolume);
             _mixerGroup.audioMixer.SetFloat(_mixerVariableName, _muteSoundsValue);
+            _slider.interactable = false;
         }
     }
 }
